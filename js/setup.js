88822ca00162997b892313113bea593f449480ad
Ana Gadelha <anabmgadelha@gmail.com>
2022-06-30 16:59:47 -0300

    update language

--- js/setup.js
+++ js/setup.js
@@ -20,7 +20,6 @@ $(function() {
         if($(window).scrollTop() > 50) {
             $(".header").addClass("header-scroll");
         } else {
-            //remove the background property so it comes transparent again (defined in your css)
            $(".header").removeClass("header-scroll");
         }
     });
